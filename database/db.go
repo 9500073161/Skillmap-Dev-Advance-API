63cb13e4f19d42043b95752800e5d0377e89e160
Prashob <89034675+9500073161@users.noreply.github.com>
2024-03-17 20:07:06 +0100

    final code

--- database/db.go
+++ database/db.go
@@ -1,13 +1,22 @@
-// package database
+package database
 
-// import (
-// 	"github.com/9500073161/Skillmap-Dev-Advance-API/models"
-// 	"gorm.io/driver/sqlite"
-// 	"gorm.io/gorm"
-// )
+import (
+	"github.com/9500073161/Skillmap-Dev-Advance-API/models"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
 
-// func Initialize(){
+func Initialize(){
+
+	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+
+	}
+
+	// Migrate the schema
+	db.AutoMigrate(&models.User{})
 
 	
 
-// }
+}
