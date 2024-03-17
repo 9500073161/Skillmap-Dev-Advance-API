603cb148bc3e581ebd0e66078da918ad5c16fb74
Prashob <89034675+9500073161@users.noreply.github.com>
2024-03-17 19:08:22 +0100

    commit-1

---
+++ database/db.go
@@ -0,0 +1,13 @@
+// package database
+
+// import (
+// 	"github.com/9500073161/Skillmap-Dev-Advance-API/models"
+// 	"gorm.io/driver/sqlite"
+// 	"gorm.io/gorm"
+// )
+
+// func Initialize(){
+
+	
+
+// }
--- main.go
+++ main.go
@@ -5,11 +5,24 @@ import (
 
 	"github.com/9500073161/Skillmap-Dev-Advance-API/handlers"
 	"github.com/9500073161/Skillmap-Dev-Advance-API/managers"
+	"github.com/9500073161/Skillmap-Dev-Advance-API/models"
 	"github.com/gin-gonic/gin"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+	
 )
 
 func main() {
 	fmt.Println("Developing Skill-MAP")
+
+	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+
+	}
+
+	// Migrate the schema
+	db.AutoMigrate(&models.User{})
 
 	router := gin.Default()
 
